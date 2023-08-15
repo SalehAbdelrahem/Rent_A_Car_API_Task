42863308e8f6adbc5e78a3790fd9912be60079bd
MhmdAbqader <Salh.15237657@compit.au.edu.eg>
2023-08-15 22:51:45 +0300

    update files

--- DAL/Models/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DAL.Models
-{
-    public class Role : IdentityRole<int>
-    {
-        public virtual ICollection<AppUser>? AppUsers { get; set; }
-    }
-}
